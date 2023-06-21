fe5956aa18ad655ca04a0ad00ae281c99225a702
blazte <blazte.aster@gmail.com>
2023-06-21 15:35:26 -0500

    config prettier and eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,24 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: ['plugin:react/recommended', 'standard', 'prettier'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint', 'prettier'],
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+		'prettier/prettier': [
+			'error',
+			{ printWidth: 100, useTabs: true, semi: false, singleQuote: true },
+		],
+	},
 }
